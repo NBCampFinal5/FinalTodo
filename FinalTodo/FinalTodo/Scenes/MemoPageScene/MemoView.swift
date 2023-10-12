a5655557fcd1111143470c226e21a331cded76b6
= <=>
2023-10-12 15:29:10 +0900

    [FEAT] 유동적인 높이를 가진 textView

--- FinalTodo/FinalTodo/Scenes/MemoPageScene/MemoView.swift
+++ FinalTodo/FinalTodo/Scenes/MemoPageScene/MemoView.swift
@@ -9,13 +9,60 @@ import UIKit
 import SnapKit
 
 class MemoView: UIView {
+    // MARK: - Property
+    
+    lazy var optionCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        
+        let view = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    lazy var contentTextView: UITextView = {
+        let view = UITextView()
+        view.isScrollEnabled = false
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    // MARK: - init
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUp()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+private extension MemoView {
+    // MARK: - SetUp
+
+    func setUp() {
+        setUpOptionCollectionView()
+        setUpContentTextView()
+    }
+    
+    func setUpOptionCollectionView() {
+        self.addSubview(optionCollectionView)
+        optionCollectionView.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(Constant.screenHeight * 0.05)
+        }
+    }
+    
+    func setUpContentTextView() {
+        self.addSubview(contentTextView)
+        contentTextView.snp.makeConstraints { make in
+            make.top.equalTo(optionCollectionView.snp.bottom).offset(Constant.defaultPadding)
+            make.height.equalTo(Constant.screenHeight * 0.05)
+            make.left.right.equalToSuperview().inset(Constant.defaultPadding)
+            make.bottom.equalToSuperview()
+        }
+    }
 }
--- FinalTodo/FinalTodo/Scenes/MemoPageScene/MemoViewController.swift
+++ FinalTodo/FinalTodo/Scenes/MemoPageScene/MemoViewController.swift
@@ -10,8 +10,7 @@ import SnapKit
 
 class MemoViewController: UIViewController {
     
-
-
+    private let memoView = MemoView()
 }
 
 extension MemoViewController {
@@ -20,12 +19,43 @@ extension MemoViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp()
+        setUpNavigation()
     }
 }
 
 private extension MemoViewController {
     // MARK: - SetUp
     func setUp() {
-        view.backgroundColor = .blue
+        setUpMemoView()
+    }
+    
+    func setUpMemoView() {
+        view.addSubview(memoView)
+        memoView.snp.makeConstraints { make in
+            make.top.left.right.equalTo(view.safeAreaLayoutGuide)
+        }
+        memoView.contentTextView.delegate = self
+    }
+    // MARK: - SetUpNavigation
+    func setUpNavigation() {
+        self.navigationItem.title = "title"
+    }
+
+}
+
+extension MemoViewController: UITextViewDelegate {
+    // MARK: - 유동적인 높이를 가진 textView
+    func textViewDidChange(_ textView: UITextView) {
+        
+        let size = CGSize(width: Constant.screenWidth - (Constant.defaultPadding * 2), height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach { (constraint) in
+            if estimatedSize.height > Constant.screenHeight * 0.05 {
+                if constraint.firstAttribute == .height {
+                    constraint.constant = estimatedSize.height
+                }
+            }
+        }
     }
 }
