13da3e60f9798ac5aa9a0a9efdaad53c1112d814
= <=>
2023-10-12 13:50:07 +0900

    [FEAT] Controller / view add

---
+++ FinalTodo/FinalTodo/Scenes/MemoPageScene/MemoView.swift
@@ -0,0 +1,21 @@
+//
+//  MemoView.swift
+//  FinalTodo
+//
+//  Created by SeoJunYoung on 2023/10/12.
+//
+
+import UIKit
+import SnapKit
+
+class MemoView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ FinalTodo/FinalTodo/Scenes/MemoPageScene/MemoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MemoViewController.swift
+//  FinalTodo
+//
+//  Created by SeoJunYoung on 2023/10/12.
+//
+
+import UIKit
+import SnapKit
+
+class MemoViewController: UIViewController {
+    
+
+
+}
+
+extension MemoViewController {
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+}
+
+private extension MemoViewController {
+    // MARK: - SetUp
+    func setUp() {
+        view.backgroundColor = .blue
+    }
+}
