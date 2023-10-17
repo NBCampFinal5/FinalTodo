70d2c6262081f44e6b5cca307af04d06fcec640d
= <=>
2023-10-18 01:47:58 +0900

    [FEAT] FirebaseManager test add

--- FinalTodo/FinalTodo/Helper/FirebaseManager.swift
+++ FinalTodo/FinalTodo/Helper/FirebaseManager.swift
@@ -4,10 +4,33 @@
 //
 //  Created by SeoJunYoung on 10/17/23.
 //
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import FirebaseDatabase
 
-import Foundation
 
+struct UserData: Codable {
+    var email: String
+    var rewardPoint: Int
+}
 
 struct FirebaseManager {
     
+    let db = Firestore.firestore()
+    let ref = Database.database().reference()
+    let user = "Users"
+    
+    func creatUserData(email: String, completion: @escaping () -> Void) {
+        
+        let userData = UserData(email: email, rewardPoint: 0)
+        
+        do {
+            let data = try Firestore.Encoder().encode(userData)
+            db.collection(user).document(email).setData(data)
+        } catch {
+            print("@@@ Fail")
+        }
+        
+    }
 }
