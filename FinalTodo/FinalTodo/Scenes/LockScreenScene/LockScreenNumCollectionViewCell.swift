18d0921193685567aa73559570d540ef7f28ac92
= <=>
2023-10-18 21:36:57 +0900

    [FEAT] LockScreen cell file add

---
+++ FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenNumCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  LockScreenNumCollectionViewCell.swift
+//  FinalTodo
+//
+//  Created by SeoJunYoung on 10/18/23.
+//
+
+import UIKit
+
+class LockScreenNumCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect.zero)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenViewController.swift
+++ FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenViewController.swift
@@ -6,24 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class LockScreenViewController: UIViewController {
+    
+    private let collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        let view = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        return view
+    }()
+}
 
+extension LockScreenViewController {
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = ColorManager.themeArray[0].backgroundColor
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
