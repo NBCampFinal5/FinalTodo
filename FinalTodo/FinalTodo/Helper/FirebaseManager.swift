261653ce157e942f7d8ea1d46fffa90e29f86e83
= <=>
2023-10-18 15:26:01 +0900

    [FEAT] Firebase Manager update, fetch 메서드 구현

--- FinalTodo/FinalTodo/Helper/FirebaseManager.swift
+++ FinalTodo/FinalTodo/Helper/FirebaseManager.swift
@@ -9,28 +9,46 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 import FirebaseDatabase
 
-
-struct UserData: Codable {
-    var email: String
-    var rewardPoint: Int
-}
-
 struct FirebaseManager {
     // TODO: - CRUD 
     let db = Firestore.firestore()
-    let ref = Database.database().reference()
-    let user = "Users"
+    let users = "Users"
     
-    func creatUserData(email: String, completion: @escaping () -> Void) {
+    func updateUserData(email: String, completion: @escaping () -> Void) {
         
-        let userData = UserData(email: email, rewardPoint: 0)
+        let userData = Users(
+            id: email,
+            nickName: "testNick2",
+            folders: [Folders(id: "hihi", name: "hihi", color: "yellow")], 
+            memoDatas: [
+                Memos(Fileid: "test1", title: "test1", date: "tset1", content: "test1", isPin: false, locationNotifySetting: "test1", timeNotifySetting: "test1"),
+                Memos(Fileid: "test", title: "test", date: "tset", content: "test", isPin: false, locationNotifySetting: "test", timeNotifySetting: "test")
+            ],
+            rewardPoint: 0,
+            settingValue: SettingValues(color: "yellow", font: "testFont")
+        )
         
         do {
             let data = try Firestore.Encoder().encode(userData)
-            db.collection(user).document(email).setData(data)
+            db.collection(users).document(email).setData(data)
         } catch {
             print("@@@ Fail")
         }
-        
+    }
+    
+    func fetchUserData(email: String, completion: @escaping (Users) -> Void) {
+        db.collection(users).document(email).getDocument { data, error in
+            if error != nil {
+                print("[FirebaseManager][\(#function)]: \(String(describing: error?.localizedDescription))")
+            } else {
+                guard let data = data?.data() else { return }
+                do {
+                    var safeData = try Firestore.Decoder().decode(Users.self, from: data)
+                    completion(safeData)
+                } catch {
+                    print("[FirebaseManager][\(#function)]: DecodingFail")
+                }
+            }
+        }
     }
 }
