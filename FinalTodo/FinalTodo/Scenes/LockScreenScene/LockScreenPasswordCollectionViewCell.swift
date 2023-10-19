de190b71016036ac19ba026656608f55755c7b04
= <=>
2023-10-19 09:48:49 +0900

    [FEAT] password cell add

---
+++ FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenPasswordCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LockScreenPasswordCollectionViewCell.swift
+//  FinalTodo
+//
+//  Created by SeoJunYoung on 10/19/23.
+//
+
+import UIKit
+
+class LockScreenPasswordCollectionViewCell: UICollectionViewCell {
+    
+}
