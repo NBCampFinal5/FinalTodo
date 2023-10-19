eb35430e7fb597db3057e17661262b0c117ba9e9
= <=>
2023-10-19 09:43:46 +0900

    [FEAT] 키패드 추가

--- FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenNumCollectionViewCell.swift
+++ FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenNumCollectionViewCell.swift
@@ -9,15 +9,36 @@ import UIKit
 
 class LockScreenNumCollectionViewCell: UICollectionViewCell {
     
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.text = "test"
+        label.textAlignment = .center
+        label.textColor = ColorManager.themeArray[0].backgroundColor
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: CGRect.zero)
-        contentView.backgroundColor = ColorManager.themeArray[0].pointColor02
-        let width = (Constant.screenWidth - (Constant.defaultPadding * 2) - (Constant.defaultPadding * 2)) / 3
-        contentView.layer.cornerRadius = width / 2
+        setUp()
+
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension LockScreenNumCollectionViewCell {
+    func setUp() {
+        contentView.backgroundColor = ColorManager.themeArray[0].pointColor01
+        let spacing = Constant.defaultPadding * 2
+        let width = (Constant.screenWidth - (Constant.defaultPadding * 6) - (spacing * 2)) / 3
+        contentView.layer.cornerRadius = width / 2
+        contentView.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
--- FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenViewController.swift
+++ FinalTodo/FinalTodo/Scenes/LockScreenScene/LockScreenViewController.swift
@@ -10,11 +10,25 @@ import SnapKit
 
 class LockScreenViewController: UIViewController {
     
+    private let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.text = "암호 입력"
+        return label
+    }()
+    
+    private let passwordCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let view = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        view.backgroundColor = .green
+        return view
+    }()
+    
     private let numsCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
-        let spacing = Constant.defaultPadding
-        let width = (Constant.screenWidth - (Constant.defaultPadding * 2) - (spacing * 2)) / 3
-        flowLayout.minimumLineSpacing = spacing
+        let spacing = Constant.defaultPadding * 2
+        let width = (Constant.screenWidth - (Constant.defaultPadding * 6) - (spacing * 2)) / 3
+        flowLayout.minimumLineSpacing = spacing / 2
         flowLayout.itemSize = .init(width: width, height: width)
         let view = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
         view.backgroundColor = .clear
@@ -34,9 +48,33 @@ private extension LockScreenViewController {
     func setUp() {
         view.backgroundColor = ColorManager.themeArray[0].backgroundColor
         view.addSubview(numsCollectionView)
+        setUpPasswordLabel()
+        setUpPasswordCollectionView()
+        setUpNumsCollectionView()
+    }
+    
+    func setUpPasswordLabel() {
+        view.addSubview(passwordLabel)
+        passwordLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(Constant.screenHeight * 0.15)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    func setUpPasswordCollectionView() {
+        view.addSubview(passwordCollectionView)
+        passwordCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(passwordLabel.snp.bottom).offset(Constant.defaultPadding)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(Constant.screenHeight * 0.05)
+        }
+    }
+    
+    func setUpNumsCollectionView() {
         numsCollectionView.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview()
-            make.left.right.equalToSuperview().inset(Constant.defaultPadding)
+            make.top.equalTo(passwordCollectionView.snp.bottom).offset(Constant.screenHeight * 0.1)
+            make.left.right.equalToSuperview().inset(Constant.defaultPadding * 3)
+            make.bottom.equalToSuperview()
         }
         numsCollectionView.delegate = self
         numsCollectionView.dataSource = self
@@ -46,14 +84,18 @@ private extension LockScreenViewController {
 
 extension LockScreenViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        12
+        return 12
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LockScreenNumCollectionViewCell.identifier, for: indexPath) as! LockScreenNumCollectionViewCell
-        
-        return cell
+        if collectionView == passwordCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LockScreenNumCollectionViewCell.identifier, for: indexPath) as! LockScreenNumCollectionViewCell
+            
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LockScreenNumCollectionViewCell.identifier, for: indexPath) as! LockScreenNumCollectionViewCell
+            
+            return cell
+        }
     }
-    
-    
 }
