5d1674f2bc9f5f6520daca823771e54518ddf77a
= <=>
2023-10-19 20:17:17 +0900

    [FEAT] 오류 해결

--- FinalTodo/FinalTodo/Helper/FirebaseManager.swift
+++ FinalTodo/FinalTodo/Helper/FirebaseManager.swift
@@ -16,23 +16,24 @@ struct FirebaseManager {
     
     func updateUserData(email: String, completion: @escaping () -> Void) {
         
-        let userData = Users(
-            id: email,
-            nickName: "testNick2",
-            folders: [Folders(id: "hihi", name: "hihi", color: "yellow")], 
-            memoDatas: [
-                Memos(Fileid: "test1", title: "test1", date: "tset1", content: "test1", isPin: false, locationNotifySetting: "test1", timeNotifySetting: "test1"),
-                Memos(Fileid: "test", title: "test", date: "tset", content: "test", isPin: false, locationNotifySetting: "test", timeNotifySetting: "test")
-            ],
-            rewardPoint: 0,
-            settingValue: SettingValues(color: "yellow", font: "testFont")
-        )
+//        let userData = Users(
+//            id: email,
+//            nickName: "testNick2",
+//            folders: [Folders(id: "hihi", name: "hihi", color: "yellow")], 
+//            memoDatas: [
+//                Memos(Fileid: "test1", title: "test1", date: "tset1", content: "test1", isPin: false, locationNotifySetting: "test1", timeNotifySetting: "test1"),
+//                Memos(Fileid: "test", title: "test", date: "tset", content: "test", isPin: false, locationNotifySetting: "test", timeNotifySetting: "test")
+//            ],
+//            rewardPoint: 0,
+//            settingValue: SettingValues(color: "yellow", font: "testFont")
+//        )
+        let userData = Users(id: "test", nickName: "tset", folders: [], memos: [], rewardPoint: 0, themeColor: 0)
         
         do {
             let data = try Firestore.Encoder().encode(userData)
             db.collection(users).document(email).setData(data)
         } catch {
-            print("@@@ Fail")
+            print("[FirebaseManager][\(#function)]:Fail")
         }
     }
     
