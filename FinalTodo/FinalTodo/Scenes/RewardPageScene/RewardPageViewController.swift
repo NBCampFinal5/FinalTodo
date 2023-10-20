2fe5ab0c6145497d30ab0d281b0120ce501dbd29
seojinkim112211 <kjn2149@naver.com>
2023-10-20 17:24:07 +0900

    [feat]기능구현

--- FinalTodo/FinalTodo/Scenes/RewardPageScene/RewardPageViewController.swift
+++ FinalTodo/FinalTodo/Scenes/RewardPageScene/RewardPageViewController.swift
@@ -10,10 +10,14 @@ import SnapKit
 
 class RewardPageViewController: UIViewController {
     
+    
+    private var score = 0
+    
     lazy var plusButton:UIButton = {
         let button = UIButton()
         button.setTitle("+1", for: .normal)
         button.setTitleColor(UIColor(named: "theme01PointColor03"), for: .normal)
+        button.addTarget(self, action: #selector(increaseScore), for: .touchUpInside)
         button.backgroundColor = .yellow
         return button
     }()
@@ -21,6 +25,7 @@ class RewardPageViewController: UIViewController {
         let button = UIButton()
         button.setTitle("-1", for: .normal)
         button.setTitleColor(UIColor(named: "theme01PointColor03"), for: .normal)
+        button.addTarget(self, action: #selector(diminishScore), for: .touchUpInside)
         button.backgroundColor = .green
         return button
     }()
@@ -35,7 +40,8 @@ class RewardPageViewController: UIViewController {
     let scoreLabel: UILabel = {
         let label = UILabel()
         label.text = "0"
-        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
         return label
         
         
@@ -44,9 +50,11 @@ class RewardPageViewController: UIViewController {
     
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "theme01PointColor02")
+        scoreLabel.text = "\(score)"
         setup()
     }
 }
@@ -72,15 +80,15 @@ private extension RewardPageViewController{
         view.addSubview(plusButton)
         plusButton.snp.makeConstraints { make in
             make.top.equalTo(giniimageView.snp.bottom).offset(Constant.screenHeight * 0.01)
-            make.centerX.equalToSuperview().offset(Constant.screenWidth * 0.01)
+            make.centerX.equalToSuperview().offset(Constant.screenWidth * 0.3)
             make.width.equalTo(50)
             make.height.equalTo(50)
         }
         
         view.addSubview(minusButton)
         minusButton.snp.makeConstraints { make in
-            make.top.equalTo(giniimageView.snp.bottom).offset(Constant.screenHeight * 0.01)
-            make.centerX.equalToSuperview().inset(Constant.screenWidth * 0.01)
+            make.top.equalTo(plusButton.snp.bottom).offset(Constant.screenHeight * 0.01)
+            make.centerX.equalToSuperview().offset(Constant.screenWidth * 0.3)
             make.width.equalTo(50)
             make.height.equalTo(50)
         }
@@ -88,15 +96,44 @@ private extension RewardPageViewController{
     func setuplabel(){
         view.addSubview(scoreLabel)
         scoreLabel.snp.makeConstraints { make in
-            make.top.equalTo(plusButton.snp.bottom)
-            make.centerX.equalToSuperview()
+            make.bottom.equalTo(plusButton.snp.top)
+            make.centerX.equalToSuperview().offset(Constant.screenWidth * 0.3)
             make.width.equalTo(50)
             make.height.equalTo(50)
         }
-        
     }
-    
 }
+
+extension RewardPageViewController {
+    
+    @objc func increaseScore() {
+        // 버튼을 누를 때 호출되는 함수
+        score += 1
+        scoreLabel.text = "\(score)"
+        if score >= 10 {
+            giniimageView.image = UIImage(named: "gini2")
+            UIView.animate(withDuration: 0.5, animations: {
+                self.giniimageView.transform = CGAffineTransform(scaleX: 2, y: 2) // 이미지 확대
+            })
+        }
+    }
+    @objc func diminishScore() {
+        // 버튼을 누를 때 호출되는 함수
+        if score > 0 {
+            score -= 1
+            scoreLabel.text = "\(score)"
+            
+            if score < 10 {
+                giniimageView.image = UIImage(named: "gini1")
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.giniimageView.transform = .identity // 이미지 원래 크기로
+                })
+            }
+        }
+    }
+}
+
+
 
 
 
