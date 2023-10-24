4b9bd11afb3a7e0a4ab3aac6a563d6871e170757
원성준 <tjdwns575859@gmail.com>
2023-10-24 21:05:11 +0900

    [FIX]: ButtonTappedView 사용하여 날짜,시간 설정 버튼 변경

--- FinalTodo/FinalTodo/SharedView/ButtonTappedView.swift
+++ FinalTodo/FinalTodo/SharedView/ButtonTappedView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ButtonTappedView: UIView {
     // 버튼
-    private lazy var anyButton: UIButton = {
+    lazy var anyButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor(named: "theme01PointColor01")
         button.layer.cornerRadius = 5
