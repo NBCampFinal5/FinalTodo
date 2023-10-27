32c1c7074df5df54a37ada2ebb5ae92fc20d4fb3
OSR <osrsla@gmail.com>
2023-10-27 19:37:56 +0900

    [FEAT] ProfilePageVC UI 수정 및 기능 추가

--- FinalTodo/FinalTodo/Scenes/SettingScene/ProfilePageScene/ProfilePageViewModel.swift
+++ FinalTodo/FinalTodo/Scenes/SettingScene/ProfilePageScene/ProfilePageViewModel.swift
@@ -8,36 +8,60 @@
 import UIKit
 
 class ProfilePageViewModel {
-//    let firebaseManager = FirebaseManager()
-    
-    var idLabelText: String = "eagle5@gmail.com" // 사용자 아이디(이메일) 받아오기
-    
-    var nickNameLabelPlaceholder: String = "짱짱독수리" // 사용자 닉네임 받아오기
-    var nickNameLabelUserInput: String = ""
-    
-    var passwordLabelPlaceholder: String = "******" // 사용자 비밀번호 받아오기
-    var passwordLabelUserInput: String = ""
+    private let manager = CoreDataManager.shared
 
-    var passwordCheckLabelPlaceholder: String = "******" // 사용자 비밀번호 받아오기
-    var passwordCheckLabelUserInput: String = ""
-    
-    var editButtonColor: Int = 1 // 사용자 지정 테마컬러 받아오기
-    
-    var allertLabelTextIsHidden: Bool = true // 텍스트필드 isHidden 불값 -> 기본값은 true, passwordLabelUserInput사용자 입력값, passwordCheckLabelUserInput 사용자 입력값이 다를 때 isHidden = false
-    
-    func fetchDataFromFirebase() {
-//        firebaseManager.fetchUserData(email: idLabelText) { [weak self] user in
-////            self?.idLabelText = user.id
-//            self?.nickNameLabelPlaceholder = user.nickName
-////            self?.passwordLabelPlaceholder = user.password // User 모델에 비밀번호 추가 필요????
-////            self?.passwordCheckLabelPlaceholder = user.password
-//            self?.editButtonColor = user.themeColor
-//        }
+    private var userId: String = "userId"
+    private var userFolders: [FolderData] = []
+    private var userFoldersMemos: [MemoData] = []
+    private var userPoint: Int32 = 0
+    private var userThemeColor = ""
+
+    var userNickName: String = "user"
+    var rewardNickName: String = "gini"
+    var giniImage: String = "gini1"
+
+    func fetchUserData() {
+        userId = manager.getUser().id
+        userFolders = manager.getUser().folders
+        userFoldersMemos = manager.getUser().memos
+        userThemeColor = manager.getUser().themeColor
+
+        userPoint = manager.getUser().rewardPoint
+        userNickName = manager.getUser().nickName // 유저 닉네임
+        rewardNickName = manager.getUser().rewardName // 리워드 닉네임
+
+        if userPoint >= 30 {
+            giniImage = "gini4"
+        } else if userPoint >= 20 {
+            giniImage = "gini3"
+        } else if userPoint >= 10 {
+            giniImage = "gini2"
+        } else {
+            giniImage = "gini1"
+        }
+        
+        print("@@ 유저 패치! user:\(userNickName), giniName: \(rewardNickName), giniImage: \(giniImage)")
+        print("@@ 현재 패치된 유저 정보:", manager.getUser())
     }
-    
-    func updateUserData() {
-//        firebaseManager.updateUserData(email: idLabelText) {
-//            print("업데이트 파이어베이스 유저데이터")
-//        }
+
+    enum EditType {
+        case userNickName
+        case rewardNickName
+    }
+
+    func updateNickName(type: EditType, newName: String) {
+        switch type {
+        case .userNickName:
+            let userWithNewNickName = UserData(id: userId, nickName: newName, folders: userFolders, memos: userFoldersMemos, rewardPoint: userPoint, rewardName: rewardNickName, themeColor: userThemeColor)
+            manager.updateUser(targetId: userId, newUser: userWithNewNickName) {
+                print("@@ 유저 닉네님 업데이트:", userWithNewNickName.nickName)
+            }
+
+        case .rewardNickName:
+            let rewardWithNewNickName = UserData(id: userId, nickName: userNickName, folders: userFolders, memos: userFoldersMemos, rewardPoint: userPoint, rewardName: newName, themeColor: userThemeColor)
+            manager.updateUser(targetId: userId, newUser: rewardWithNewNickName) {
+                print("@@ 리워드 닉네임 업데이트:", rewardWithNewNickName.rewardName)
+            }
+        }
     }
 }
