6349fbdd7a7d53874e1a85cc7901a2aa1029c38a
OSR <osrsla@gmail.com>
2023-11-06 15:12:44 +0900

    [FEAT] 검색 메모 리스트와 메모 수정 화면 연결

--- FinalTodo/FinalTodo/Scenes/MainPageScene/SearchViewScene/SearchViewController.swift
+++ FinalTodo/FinalTodo/Scenes/MainPageScene/SearchViewScene/SearchViewController.swift
@@ -115,6 +115,10 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             searchBar.resignFirstResponder()
         }
         tableView.deselectRow(at: indexPath, animated: true) // 셀 선택상태 해제(셀 터치시 한번만 터치되게끔)
+        let selectedMemo = viewModel.filterData.value[indexPath.row]
+        let editMemoVC = MemoViewController()
+        editMemoVC.loadMemoData(memo: selectedMemo)
+        navigationController?.pushViewController(editMemoVC, animated: true)
     }
 }
 
