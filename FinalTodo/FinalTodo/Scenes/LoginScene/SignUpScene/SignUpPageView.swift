8c524a91159ba81339a3f1277a18eb2890249c78
= <=>
2023-11-10 18:19:28 +0900

    [FEAT] ViewFile 구현

--- FinalTodo/FinalTodo/Scenes/LoginScene/SignUpScene/SignUpPageView.swift
+++ FinalTodo/FinalTodo/Scenes/LoginScene/SignUpScene/SignUpPageView.swift
@@ -5,16 +5,16 @@
 //  Created by SeoJunYoung on 11/10/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
 
-class SignUpPageView: UIView {
-    
-    // 맨위에 굵은글자
-    private lazy var registerLabel: UILabel = {
+final class SignUpPageView: UIView {
+    // MARK: - Property
+    private let registerLabel: UILabel = {
         let label = UILabel()
         label.text = "가입하기"
         label.textColor = .label
-        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
         return label
     }()
     
@@ -24,14 +24,12 @@ class SignUpPageView: UIView {
     let checkPasswordTextField = CommandLabelView(title: "비밀번호 확인", placeholder: "패스워드를 재입력해 주세요.", isSecureTextEntry: true)
     let registerButton = ButtonTappedView(title: "가입하기")
     
-    lazy var privacyPolicyButton: UIButton = {
+    let privacyPolicyButton: UIButton = {
         let button = UIButton()
         button.setTitle("개인정보 처리방침에 동의합니다. ", for: .normal)
         let checkboxImage = UIImage(systemName: "square")
         button.setImage(checkboxImage, for: .normal)
         button.imageView?.contentMode = .scaleAspectFit
-        
-        button.addTarget(self, action: #selector(checkboxTapped), for: .touchUpInside)
         button.setTitleColor(.secondaryLabel, for: .normal)
         button.tintColor = .secondaryLabel
         button.backgroundColor = .systemBackground
@@ -39,6 +37,7 @@ class SignUpPageView: UIView {
         
         return button
     }()
+    
     let linkButton: UIButton = {
         let button = UIButton()
         button.setTitle("[보기]", for: .normal)
@@ -47,4 +46,98 @@ class SignUpPageView: UIView {
         return button
     }()
     
+    // MARK: - 생성자
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension SignUpPageView {
+    // MARK: - SetUp
+    
+    func setUp() {
+        setUpRegisterLabel()
+        setUpEmailTextField()
+        setUpNickNameTextField()
+        setUpPasswordTextField()
+        setUpCheckPasswordTextField()
+        setUpPrivacyPolicyButton()
+        setUpLinkButton()
+        setUpRegisterButton()
+    }
+    
+    func setUpRegisterLabel() {
+        self.addSubview(registerLabel)
+        registerLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(Constant.screenHeight * 0.05)
+            make.leading.equalTo(Constant.defaultPadding)
+        }
+    }
+    
+    func setUpEmailTextField() {
+        self.addSubview(emailTextField)
+        emailTextField.addInfoLabel()
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(registerLabel.snp.bottom).offset(Constant.screenHeight * 0.02)
+            make.leading.trailing.equalToSuperview().inset(Constant.defaultPadding)
+        }
+    }
+    
+    func setUpNickNameTextField() {
+        self.addSubview(nicknameTextField)
+        nicknameTextField.addInfoLabel()
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(Constant.screenHeight * 0.02)
+            make.left.right.equalToSuperview().inset(Constant.defaultPadding)
+        }
+    }
+    
+    func setUpPasswordTextField() {
+        self.addSubview(passwordTextField)
+        passwordTextField.addInfoLabel()
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(Constant.screenHeight * 0.02)
+            make.leading.trailing.equalToSuperview().inset(Constant.defaultPadding)
+        }
+    }
+    
+    func setUpCheckPasswordTextField() {
+        self.addSubview(checkPasswordTextField)
+        checkPasswordTextField.addInfoLabel()
+        checkPasswordTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(Constant.screenHeight * 0.02)
+            make.leading.trailing.equalToSuperview().inset(Constant.defaultPadding)
+        }
+    }
+    
+    func setUpPrivacyPolicyButton() {
+        self.addSubview(privacyPolicyButton)
+        privacyPolicyButton.snp.makeConstraints { make in
+            make.top.equalTo(checkPasswordTextField.snp.bottom).offset(Constant.screenHeight * 0.02)
+            make.left.equalTo(checkPasswordTextField.snp.left).inset(Constant.defaultPadding)
+        }
+    }
+    
+    func setUpLinkButton() {
+        self.addSubview(linkButton)
+        linkButton.snp.makeConstraints { make in
+            make.centerY.equalTo(privacyPolicyButton.snp.centerY)
+            make.left.equalTo(privacyPolicyButton.snp.right).offset(Constant.defaultPadding / 2)
+        }
+    }
+    
+    func setUpRegisterButton() {
+        // 버튼
+        self.addSubview(registerButton)
+        registerButton.snp.makeConstraints { make in
+            make.top.equalTo(checkPasswordTextField.snp.bottom).offset(Constant.screenHeight * 0.07)
+            make.leading.trailing.equalToSuperview().inset(Constant.defaultPadding)
+        }
+    }
+
 }
