f218a08e69a08821291f3ccaf6a1ebda67cf425a
= <=>
2023-11-14 20:54:43 +0900

    [FEAT] RewardPageView Add

---
+++ FinalTodo/FinalTodo/Scenes/RewardPageScene/RewardPageView.swift
@@ -0,0 +1,28 @@
+//
+//  RewardPageView.swift
+//  FinalTodo
+//
+//  Created by SeoJunYoung on 11/14/23.
+//
+
+import UIKit
+
+class RewardPageView: UIView {
+
+    
+    // MARK: - 생성자
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension RewardPageView {
+    func setUp() {
+        
+    }
+}
